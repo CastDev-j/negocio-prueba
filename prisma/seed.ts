447b6753769f993718dfd8e9f561392cdcd5c356
AndDev <23031429@itcelaya.edu.mx>
2025-04-27 09:13:43 -0600

    feat: update package.json to remove turbopack, add prisma seed script, and change bcrypt to bcryptjs
    feat(prisma): add User model with Role enum and seed script for initial users
    
    chore: create prisma.ts for PrismaClient initialization

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { Prisma } from "@/app/generated/prisma";
+import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: "test1@google.com",
+    name: "Test User 1",
+    password: bcrypt.hashSync("123456", 10),
+    role: "admin",
+    image: "https://mx.pinterest.com/pin/1266706140449957/",
+  },
+  {
+    email: "test2@google.com",
+    name: "Test User 2",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export async function main() {
+  if (process.env.NODE_ENV === "production") {
+    throw new Error("You are trying to seed the production database.");
+  }
+
+  try {
+    await prisma.user.deleteMany();
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  }
+
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
