9875b05bf939a751ff4a65ad4e035df439b7afe6
AndDev <23031429@itcelaya.edu.mx>
2025-04-27 12:25:25 -0600

    feat: remove user existence check and logging from saveCredentialUser function

--- src/app/actions/saveCredentialUser.ts
+++ src/app/actions/saveCredentialUser.ts
@@ -6,18 +6,7 @@ import { RegisterFormValues } from "@/lib/schemas/finance-schemas";
 import { redirect } from "next/navigation";
 
 export const saveCredentialUser = async (data: RegisterFormValues) => {
-  console.log("Iniciando búsqueda de usuario...");
-
-  const existingUser = await prisma.user.findUnique({
-    where: { email: data.email },
-  });
-
-  console.log("Búsqueda de usuario completada");
-
-  if (existingUser) {
-    return redirect("/auth/register?error=EmailAlreadyExists");
-  }
-
+    
   const newUser = await prisma.user.create({
     data: {
       name: data.name,
