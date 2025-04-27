4999913fafc486c492392a72a376a0a9ffd64dc0
AndDev <23031429@itcelaya.edu.mx>
2025-04-27 17:56:29 -0600

    fix: remove withPrismaPlugin integration and update package dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import { withPrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+ 
 };
 
-export default withPrismaPlugin(nextConfig);
+export default nextConfig;
