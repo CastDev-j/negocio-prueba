314c20b8e81eb9801c33fee091c7faa193536b5b
AndDev <23031429@itcelaya.edu.mx>
2025-04-27 12:19:27 -0600

    feat: add logging for user search process in saveCredentialUser function

--- src/app/actions/saveCredentialUser.ts
+++ src/app/actions/saveCredentialUser.ts
@@ -6,9 +6,13 @@ import { RegisterFormValues } from "@/lib/schemas/finance-schemas";
 import { redirect } from "next/navigation";
 
 export const saveCredentialUser = async (data: RegisterFormValues) => {
+  console.log("Iniciando búsqueda de usuario...");
+
   const existingUser = await prisma.user.findUnique({
     where: { email: data.email },
   });
+
+  console.log("Búsqueda de usuario completada");
 
   if (existingUser) {
     return redirect("/auth/register?error=EmailAlreadyExists");
