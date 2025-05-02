549513d39532ca5bf19267917efdab5d8a1b86f5
AndDev <23031429@itcelaya.edu.mx>
2025-05-02 15:57:43 -0600

    feat: implement UsersLoading and UsersComponent for enhanced user report loading states; add user data fetching and management features

---
+++ src/app/(dashboard)/admin/reportes/page.tsx
@@ -0,0 +1,33 @@
+import { getUsers } from "@/app/actions/users/users";
+import { UsersComponent } from "./ui/user-component";
+import { Metadata } from "next";
+import { auth } from "@/auth";
+
+export const metadata: Metadata = {
+  title: "FinanzaPyme - Reportes Avanzados",
+  description: "Aquí puedes ver los reportes avanzados de tu negocio",
+};
+
+export default async function UserPage() {
+  const session = await auth();
+
+  const { success, data } = (await getUsers()) || [];
+
+  const users = success ? data : [];
+
+  return (
+    <div className="flex flex-col mx-auto py-6 w-full">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold tracking-tight">
+          Reportes Avanzados
+        </h1>
+        <p className="text-muted-foreground mt-2">
+          Aquí puedes ver los reportes de tu negocio. y puedes imprimir formatos
+          especiales.
+        </p>
+      </div>
+
+      <UsersComponent users={users!} session={session!} />
+    </div>
+  );
+}
